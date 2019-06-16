22299460db90b78bb4450373737447d7eb730887
nikotan <niko25pu@gmail.com>
2019-06-16 22:29:10 +0900

    update algorithm and parameters for new washing machine

--- arucotest/ar.py
+++ arucotest/ar.py
@@ -6,17 +6,17 @@ import cv2
 import numpy as np
 
 if len(sys.argv) > 1:
-  filename = sys.argv[1]
+    filename = sys.argv[1]
 
 aruco = cv2.aruco
 dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
 
 size = 20
 margin = 2
-m0_offset_x = 4
-m0_offset_y = 5
-m0_width = 25
-m0_height = 18
+m0_offset_x = -32
+m0_offset_y = 10
+m0_width = 30
+m0_height = 24
 m1_offset_x = -29
 m1_offset_y = 20
 m1_width = 40
@@ -26,39 +26,41 @@ path, ext = os.path.splitext(filename)
 
 img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
-aruco.drawDetectedMarkers(img, corners, ids, (0,255,0))
+print(ids)
+aruco.drawDetectedMarkers(img, corners, ids, (0, 255, 0))
 cv2.imwrite(path + '_out' + ext, img)
 
 finished = False
 
 for mid, mcorner in zip(ids, corners):
-  pts1 = np.float32(mcorner)
-  pts2 = np.float32([
-    [size*margin,size*margin],
-    [size*(margin+1),size*margin],
-    [size*(margin+1),size*(margin+1)],
-    [size*margin,size*(margin+1)]])
+    pts1 = np.float32(mcorner)
+    pts2 = np.float32([
+        [size * margin, size * margin],
+        [size * (margin + 1), size * margin],
+        [size * (margin + 1), size * (margin + 1)],
+        [size * margin, size * (margin + 1)]])
 
-  M = cv2.getPerspectiveTransform(pts1,pts2)
-  dst = cv2.warpPerspective(img, M, (size*(margin*2+1),size*(margin*2+1)))
+    M = cv2.getPerspectiveTransform(pts1, pts2)
+    dst = cv2.warpPerspective(
+        img, M, (size * (margin * 2 + 1), size * (margin * 2 + 1)))
+    cv2.imwrite(path + '_dst' + ext, dst)
 
-  if mid[0] == 0:
-    msk = dst[
-      size*(margin+1)+m0_offset_y : size*(margin+1)+m0_offset_y+m0_height,
-      size*(margin+1)+m0_offset_x : size*(margin+1)+m0_offset_x+m0_width
-    ]
-    cv2.imwrite('%s_%s_result%s' % (path, mid[0], ext), msk)
-    if np.sum(msk > 128) * 1.0 / msk.size > 0.2:
-      finished = True
+    if mid[0] == 0:
+        msk = dst[
+            size * margin + m0_offset_y: size * margin + m0_offset_y + m0_height,
+            size * margin + m0_offset_x: size * margin + m0_offset_x + m0_width
+        ]
+        cv2.imwrite('%s_%s_result%s' % (path, mid[0], ext), msk)
+        if np.sum(msk > 128) * 1.0 / msk.size > 0.2:
+            finished = True
 
-  elif mid[0] == 1:
-    cv2.imwrite(
-      '%s_%s_result%s' % (path, mid[0], ext),
-      dst[
-        size*(margin+1)+m1_offset_y : size*(margin+1)+m1_offset_y+m1_height,
-        size*(margin+1)+m1_offset_x : size*(margin+1)+m1_offset_x+m1_width
-      ]
-    )
+    elif mid[0] == 1:
+        cv2.imwrite(
+            '%s_%s_result%s' % (path, mid[0], ext),
+            dst[
+                size * (margin + 1) + m1_offset_y: size * (margin + 1) + m1_offset_y + m1_height,
+                size * (margin + 1) + m1_offset_x: size * (margin + 1) + m1_offset_x + m1_width
+            ]
+        )
 
-print finished
-
+print(finished)
--- monitor.py
+++ monitor.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-import sys, os, shutil
-import urllib, urllib2, json
+import sys
+import os
+import shutil
+import json
+import urllib.parse
+import urllib.request
 from datetime import datetime
 import cv2
 import numpy as np
@@ -11,231 +15,168 @@ IS_DEBUG = False
 # USBカメラからカラー画像を取得
 #   返り値：カラー画像(dtype=float32)
 def captureImage(param):
-  cap = cv2.VideoCapture(param["capture"]["cam_port"])
-  cap.set(cv2.CAP_PROP_FRAME_WIDTH,  param["capture"]["cam_width"])
-  cap.set(cv2.CAP_PROP_FRAME_HEIGHT, param["capture"]["cam_height"])
-  
-  for i in xrange(param["capture"]["frames_skip"]):
-    cap.read()
-  
-  image = np.zeros((param["capture"]["cam_height"], param["capture"]["cam_width"], 3), np.float32)
-  cnt = 0
-  for i in xrange(param["capture"]["frames_capture"]):
-    ret, im = cap.read()
-    if ret == True:
-      cnt += 1
-      image += im
-  
-  cap.release()
+    cap = cv2.VideoCapture(param["capture"]["cam_port"])
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,  param["capture"]["cam_width"])
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, param["capture"]["cam_height"])
 
-  if cnt > 0:
-    image /= cnt
+    for i in range(param["capture"]["frames_skip"]):
+        cap.read()
 
-  return image
+    image = np.zeros((param["capture"]["cam_height"],
+                      param["capture"]["cam_width"], 3), np.float32)
+    cnt = 0
+    for i in range(param["capture"]["frames_capture"]):
+        ret, im = cap.read()
+        if ret == True:
+            cnt += 1
+            image += im
+
+    cap.release()
+
+    if cnt > 0:
+        image /= cnt
+
+    return image
 
 
 # 入力画像からマーカを基準に7セグLED領域を見つけて切り出し
 #   imgはグレースケール(dtype=uint8)
 def cropPatchImages(img, param):
-  aruco = cv2.aruco
-  dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
-  corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
-  
-  if IS_DEBUG:
-    img_marker = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    aruco.drawDetectedMarkers(img_marker, corners, ids, (0,255,0))
-    cv2.imwrite('debug_marker.jpg', img_marker)
+    aruco = cv2.aruco
+    dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    corners, ids, rejectedImgPoints = aruco.detectMarkers(img, dictionary)
 
-  patch_pc = None
-  patch_min = None
-  
-  if ids is not None:
-    sMkr = param['marker']['size']
-    sFrm = sMkr * param['marker']['nFrame']
-    sOut = 2 * sFrm + sMkr
-
-    for mid, mcorner in zip(ids, corners):
-      pts1 = np.float32(mcorner)
-      pts2 = np.float32([
-        [sFrm, sFrm],
-        [sFrm + sMkr, sFrm],
-        [sFrm + sMkr, sFrm + sMkr],
-        [sFrm, sFrm + sMkr]])
-      
-      M = cv2.getPerspectiveTransform(pts1,pts2)
-      dst = cv2.warpPerspective(img, M, (sOut, sOut))
-      
-      #終了判定用マーカー
-      if mid[0] == 0:
-        #マーカ座標を基準に7セグLED領域を切り出し
-        patch_x = sFrm + param['marker_pc']['offset_x']
-        patch_y = sFrm + param['marker_pc']['offset_y']
-        patch_pc = dst[
-          patch_y : patch_y + param['marker_pc']['height'],
-          patch_x : patch_x + param['marker_pc']['width']
-        ]
-
-      #残り時間判定用マーカー
-      elif mid[0] == 1:
-        #マーカ座標を基準に7セグLED領域を切り出し
-        patch_x = sFrm + param['marker_min']['offset_x']
-        patch_y = sFrm + param['marker_min']['offset_y']
-        patch_min = dst[
-          patch_y : patch_y + param['marker_min']['height'],
-          patch_x : patch_x + param['marker_min']['width']
-        ]
-
-  return patch_pc, patch_min
-
-
-# 入力画像から7セグLED領域矩形を取得
-#   imgはグレースケール(dtype=uint8)
-def getBoundary(img):
-  ave = np.average(img)
-  img_blur = cv2.GaussianBlur(img,(5,5),0)
-  img_blur[img_blur > ave] = ave
-  th, img_bin = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-  if IS_DEBUG:
-    cv2.imwrite('debug_patch_blur.jpg', img_blur)
-    cv2.imwrite('debug_patch_bin.jpg', img_bin)
-
-  img_bin = cv2.bitwise_not(img_bin)
-  ret, contours, hierarchy = cv2.findContours(img_bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-  
-  s_max = -1
-  x0 = -1
-  y0 = -1
-  x1 = -1
-  y1 = -1
-  for contour in contours:
-    x,y,w,h = cv2.boundingRect(contour)
-    s = w * h
-    if s > s_max:
-      s_max = s
-      x0 = x
-      y0 = y
-      x1 = x + w
-      y1 = y + h
-
-  return x0, y0, x1, y1, th
-
-
-# 画像パッチから洗濯終了判定(輝度が大きい場合には終了と判定)
-#   imgはグレースケール(dtype=uint8)
-#   返り値：-1=マーカ未発見、0=洗濯非終了、1=洗濯終了
-def isFinished(img):
-  if img is None:
-    return -1, img, 0.0
-  else:
-    x0, y0, x1, y1, th = getBoundary(img)
-    img_blur = cv2.GaussianBlur(img,(5,5),0)
-    msk_blur = img_blur[y0 : y1, x0 : x1]
-    th_msk, msk_bin = cv2.threshold(msk_blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     if IS_DEBUG:
-      cv2.imwrite('debug_msk_blur.jpg', msk_blur)
-      cv2.imwrite('debug_msk_bin.jpg', msk_bin)
-      print("th = %s, th_msk = %s" % (th, th_msk))
-    
-    if th_msk < 80.0:
-      th_msk = 80.0
-    ratio = np.sum(msk_blur > th_msk) * 1.0 / msk_blur.size
-    if IS_DEBUG:
-      print("--> th_msk = %s" % (th_msk))
-      print("    ratio = %s" % (ratio))
+        img_marker = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        aruco.drawDetectedMarkers(img_marker, corners, ids, (0, 255, 0))
+        cv2.imwrite('debug_marker.jpg', img_marker)
 
-    tmp = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-    cv2.rectangle(tmp, (x0, y0), (x1-1, y1-1), (0,0,255), 1)
-    cv2.putText(tmp, "%2.2f" % (ratio), (x0, y1), cv2.FONT_HERSHEY_PLAIN, 1.0, (0,0,255))
+    patch_pc = None
+    patch_min = None
 
-    if ratio > 0.2:
-      return 1, tmp, ratio
+    if ids is not None:
+        sMkr = param['marker']['size']
+        sFrm = sMkr * param['marker']['nFrame']
+        sOut = 2 * sFrm + sMkr
+
+        for mid, mcorner in zip(ids, corners):
+            pts1 = np.float32(mcorner)
+            pts2 = np.float32([
+                [sFrm, sFrm],
+                [sFrm + sMkr, sFrm],
+                [sFrm + sMkr, sFrm + sMkr],
+                [sFrm, sFrm + sMkr]])
+
+            M = cv2.getPerspectiveTransform(pts1, pts2)
+            dst = cv2.warpPerspective(img, M, (sOut, sOut))
+
+            # 終了判定用マーカー
+            if mid[0] == 0:
+                # マーカ座標を基準に7セグLED領域を切り出し
+                patch_x = sFrm + param['marker_pc']['offset_x']
+                patch_y = sFrm + param['marker_pc']['offset_y']
+                patch_pc = dst[
+                    patch_y: patch_y + param['marker_pc']['height'],
+                    patch_x: patch_x + param['marker_pc']['width']
+                ]
+
+    return patch_pc
+
+
+# 画像パッチから電源状態判定(輝度が大きい場合には電源ONと判定)
+#   imgはグレースケール(dtype=uint8)
+#   返り値：-1=マーカ未発見、0=輝度小(電源OFF)、1=輝度大(電源ON)
+def isPowerOn(img):
+    if img is None:
+        return -1, img, 0.0
     else:
-      return 0, tmp, ratio
+        h, w = img.shape[:2]
+        #img_blur = cv2.GaussianBlur(img,(5,5),0)
+        ratio = np.sum(img > 128) * 1.0 / img.size
+        tmp = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        cv2.putText(tmp, "%2.2f" % (ratio), (1, h-1),
+                    cv2.FONT_HERSHEY_PLAIN, 1.0, (0, 0, 255))
+
+        if ratio > 0.2:
+            return 1, tmp, ratio
+        else:
+            return 0, tmp, ratio
 
 
 # iftttでイベント送信
-def sendIftttEvent(ratio):
-  value = "%0.2f" % (ratio)
-  obj = {"value1" : value}
-  json_data = json.dumps(obj).encode("utf-8")
-  
-  data = urllib.urlencode(obj);
-  request = urllib2.Request(param['ifttt']['url'], data)
-  response = urllib2.urlopen(request)
+def sendIftttEvent(ratioNow, ratioOld):
+    value = "{:.2f} -> {:.2f}".format(ratioOld, ratioNow)
+    obj = {"value1": value}
+    json_data = json.dumps(obj).encode("utf-8")
+    #data = urllib.parse.urlencode(obj)
+    headers = {"Content-Type" : "application/json"}
+
+    request = urllib.request.Request(param['ifttt']['url'], data=json_data, headers=headers)
+    with urllib.request.urlopen(request) as res:
+        body = res.read().decode("utf-8")
 
 
+if __name__ == "__main__":
 
-if __name__ == "__main__": 
+    # 設定を取得
+    if not os.path.exists('init.json'):
+        print("error!")
+        sys.exit(1)
+    param = json.load(open('init.json', 'r'))
 
-  # 設定を取得
-  if not os.path.exists('init.json'):
-    print "error!"
-    sys.exit(1)
-  param = json.load(open('init.json', 'r'))
-
-  # 処理履歴を取得
-  if os.path.exists('log.json'):
-    log = json.load(open('log.json', 'r'))
-  else:
-    log = {}
-    now = datetime.now()
-    log['datetime'] = now.strftime("%Y/%m/%d %H:%M:%S")
-    log['unixtime'] = long(datetime.now().strftime('%s'))
-    log['count'] = -1
-
-  img = None
-  if len(sys.argv) > 1:
-    # 引数があった場合(テスト用)
-    filename = sys.argv[1]
-    path, ext = os.path.splitext(filename)
-    img = cv2.imread(filename, cv2.IMREAD_COLOR)
-  else:
-    # 引数がなかった場合(カメラから画像を取得)
-    img = captureImage(param)
-  cv2.imwrite('log_image.jpg', img)
-
-  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  gray = np.uint8(gray)
-
-  # 7セグLED領域矩形を取得
-  patch_pc, patch_min = cropPatchImages(gray, param)
-  if patch_pc is not None:
-    cv2.imwrite('log_patch_pc.jpg', patch_pc)
-  if patch_min is not None:
-    cv2.imwrite('log_patch_min.jpg', patch_min)
-
-  # 洗濯終了を判定
-  finished, patch_pc_b, ratio = isFinished(patch_pc)
-  if patch_pc_b is not None:
-    cv2.imwrite('log_patch_pc_b.jpg', patch_pc_b)
-  
-  # 現在時刻を取得
-  now = datetime.now()
-  now_ut = long(now.strftime('%s'))
-  now_dt = now.strftime("%Y/%m/%d %H:%M:%S")
-  now_fn = now.strftime("%Y%m%d-%H%M%S")
-
-  # 終了判定結果に基づいて通知
-  if finished == 1:
-    if log['count'] == -1:
-      if IS_DEBUG is False:
-        sendIftttEvent(ratio)
-      log['datetime'] = now_dt
-      log['unixtime'] = now_ut
-      log['count'] = 1
+    # 処理履歴を取得
+    if os.path.exists('log.json'):
+        log = json.load(open('log.json', 'r'))
     else:
-      dsec = now_ut - log['unixtime']
-      dsec_th = log['count'] * param['notify']['interval_sec']
-      if log['count'] < param['notify']['max_count'] and dsec > dsec_th:
-        sendIftttEvent(ratio)
-        log['count'] = 1 + dsec / param['notify']['interval_sec']
-    shutil.copy('log_image.jpg', 'log/%s.jpg' % (now_fn))
-    shutil.copy('log_patch_pc.jpg', 'log/%s_pc.jpg' % (now_fn))
-    shutil.copy('log_patch_pc_b.jpg', 'log/%s_pc_b.jpg' % (now_fn))
-  elif finished == 0:
-    log['count'] = -1
+        log = {}
+        now = datetime.now()
+        log['datetime'] = now.strftime("%Y/%m/%d %H:%M:%S")
+        log['unixtime'] = int(now.timestamp())
+        log['ratio'] = 0.0
+        log['powerOn'] = -1
 
-  # 処理履歴を保存
-  f = open('log.json', 'w')
-  json.dump(log, f, indent=2)
-  
+    img = None
+    if len(sys.argv) > 1:
+        # 引数があった場合(テスト用)
+        filename = sys.argv[1]
+        path, ext = os.path.splitext(filename)
+        img = cv2.imread(filename, cv2.IMREAD_COLOR)
+    else:
+        # 引数がなかった場合(カメラから画像を取得)
+        img = captureImage(param)
+    cv2.imwrite('log_image.jpg', img)
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = np.uint8(gray)
+
+    # 7セグLED領域矩形を取得
+    patch_pc = cropPatchImages(gray, param)
+    if patch_pc is not None:
+        cv2.imwrite('log_patch_pc.jpg', patch_pc)
+
+    # 洗濯機の電源状態を判定
+    powerOn, patch_pc_b, ratio = isPowerOn(patch_pc)
+    if patch_pc_b is not None:
+        cv2.imwrite('log_patch_pc_b.jpg', patch_pc_b)
+
+    # 現在時刻を取得
+    now = datetime.now()
+    now_ut = int(now.timestamp())
+    now_dt = now.strftime("%Y/%m/%d %H:%M:%S")
+    now_fn = now.strftime("%Y%m%d-%H%M%S")
+
+    # 終了判定結果に基づいて通知
+    if powerOn == 0:
+        if log['powerOn'] == 1:
+            if IS_DEBUG is False:
+                sendIftttEvent(ratio, log['ratio'])
+        log['powerOn'] = 0
+    elif powerOn == 1:
+        log['powerOn'] = 1
+    log['datetime'] = now_dt
+    log['unixtime'] = now_ut
+    log['ratio'] = ratio
+
+    # 処理履歴を保存
+    f = open('log.json', 'w')
+    json.dump(log, f, indent=2)
